d7361afe8a5e3f822f74ea3c9ffdf884d77b2a87
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-04-15 14:32:03 -0400

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,5 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node }, rules: { "semi": ["error", "always"], "no-unused-vars": "warn" } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node }, rules: { "semi": ["error", "always"], "no-unused-vars": "warn", "camelcase": ["error", { "properties": "always" }], "indent": ["error", 4] } },
 ]);
